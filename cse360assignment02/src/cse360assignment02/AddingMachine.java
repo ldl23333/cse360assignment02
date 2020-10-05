1ee563412f89c3b68650c1c51bb1fc2a58270df6
Donglai Lyu <ldl@localhost>
2020-10-06 04:16:30 +0800

    First change.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -6,25 +6,35 @@ package cse360assignment02;
  */
 public class AddingMachine {
   private int total;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
-  public void add (int value) {
+  public void add (int value) 
+  {
+	  total = total + value;
+	  history += "+"+value;
   }
 
-  public void subtract (int value) {
+  public void subtract (int value) 
+  {
+	  total = total - value;
+	  history += "-"+value;
   }
 
   public String toString () {
-    return "";
+    return history;
   }
 
-  public void clear() {
+  public void clear() 
+  {
+	  history = "0";
   }
 }
